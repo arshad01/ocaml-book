90aabad0f2f489df3259b008294753ce31b48d1f
arshad01 <khan.m.arshad@gmail.com>
2016-06-01 09:42:33 +0800

    Build alternate assoc list impl

--- chapter14/build.sh
+++ chapter14/build.sh
@@ -1,10 +1,18 @@
 echo "Building Association List"
 echo
-ocamlc -o alist alist.ml
+ocamlc -o alist2 alist2.ml
 
 echo "Running Association List"
+./alist2
+
+echo
+echo "Building Association List (with functor)"
+echo
+ocamlc -o alist alist.ml
+
+echo "Running Association List (with functor)"
 ./alist
 
 echo
 echo "Cleaning up..."
-rm alist *.cmo *.cmi
+rm alist alist2 *.cmo *.cmi
